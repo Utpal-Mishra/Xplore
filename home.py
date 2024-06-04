ba618e789810611dedc8a6d9268c97bca0764b9b
Utpal Mishra <utpalmishra21official@gmail.com>
2024-06-04 16:37:59 +0100

    Update Code

--- home.py
+++ home.py
@@ -49,169 +49,171 @@ def app():
     
     st.header("SO, WHAT DO WE HAVE AROUND?")
     
-    address = st.text_input("\nEnter Location: ", 'Dublin, Ireland')
+    address = st.text_input("\nEnter Location: ")
+    
+    if address:
 
-    geolocator = Nominatim(user_agent="four_square")
-    location = geolocator.geocode(address)
-    latitude = location.latitude
-    longitude = location.longitude
-    
-    # st.write('\nThe geograpical coordinate of {} are {}, {}.'.format(address, latitude, longitude))
-      
-      
-    """
-    Map = folium.Map(location = [latitude, longitude], zoom_start = 12, tiles = 'Stamen Terrain')
-    
-    Marker = folium.map.FeatureGroup()
-    Marker.add_child(folium.CircleMarker([latitude, longitude],
-                                                radius = 5,
-                                                color = 'red',
-                                                fill_color = 'Red'))
-    Map.add_child(Marker)
-    folium.Marker([latitude, longitude], popup = address).add_to(Map)
-    
-    st.map(Map, zoom = 7.5)
-    """
-    
-    
-    def get_category_type(row):
-        try:
-            categories_list = row['categories']
-        except:
-            categories_list = row['venue.categories']
-
-        if len(categories_list) == 0:
-            return None
-        else:
-            return categories_list[0]['name']
+        geolocator = Nominatim(user_agent="four_square")
+        location = geolocator.geocode(address)
+        latitude = location.latitude
+        longitude = location.longitude
+        
+        # st.write('\nThe geograpical coordinate of {} are {}, {}.'.format(address, latitude, longitude))
         
         
-    CLIENT_ID = 'BF0HGS24CHNDJE2XJ5QH5H0UFDUTIGYZ0Y4JJCREDG0Z0PE4'
-    CLIENT_SECRET = 'JO5OD3ZLJSQSR5UAJNQXY2DJP1RTEHCQDWJAWQIM5PWTNHYR'
-    VERSION = '20180604'
-    
-    
-    LIMIT = 500
-    radius = st.text_input("\nEnter Radius: ", '1000')
+        """
+        Map = folium.Map(location = [latitude, longitude], zoom_start = 12, tiles = 'Stamen Terrain')
+        
+        Marker = folium.map.FeatureGroup()
+        Marker.add_child(folium.CircleMarker([latitude, longitude],
+                                                    radius = 5,
+                                                    color = 'red',
+                                                    fill_color = 'Red'))
+        Map.add_child(Marker)
+        folium.Marker([latitude, longitude], popup = address).add_to(Map)
+        
+        st.map(Map, zoom = 7.5)
+        """
+        
+        
+        def get_category_type(row):
+            try:
+                categories_list = row['categories']
+            except:
+                categories_list = row['venue.categories']
 
-    url = 'https://api.foursquare.com/v2/venues/explore?&client_id={}&client_secret={}&v={}&ll={},{}&radius={}&limit={}'.format(CLIENT_ID, CLIENT_SECRET, VERSION, latitude, longitude, radius, LIMIT)
-    result = requests.get(url).json()
+            if len(categories_list) == 0:
+                return None
+            else:
+                return categories_list[0]['name']
+            
+            
+        CLIENT_ID = 'BF0HGS24CHNDJE2XJ5QH5H0UFDUTIGYZ0Y4JJCREDG0Z0PE4'
+        CLIENT_SECRET = 'JO5OD3ZLJSQSR5UAJNQXY2DJP1RTEHCQDWJAWQIM5PWTNHYR'
+        VERSION = '20180604'
+        
+        
+        LIMIT = 500
+        radius = st.text_input("\nEnter Radius: ", '1000')
 
-    venues = result['response']['groups'][0]['items']
-    venues = json_normalize(venues)
-    #venues.head()
-    data = pd.DataFrame(venues)
-    data.shape
+        url = 'https://api.foursquare.com/v2/venues/explore?&client_id={}&client_secret={}&v={}&ll={},{}&radius={}&limit={}'.format(CLIENT_ID, CLIENT_SECRET, VERSION, latitude, longitude, radius, LIMIT)
+        result = requests.get(url).json()
+
+        venues = result['response']['groups'][0]['items']
+        venues = json_normalize(venues)
+        #venues.head()
+        data = pd.DataFrame(venues)
+        data.shape
 
 
-    data = data[['venue.name', 'venue.location.address', 'venue.location.lat', 'venue.location.lng', 'venue.location.distance', 'venue.location.city', 'venue.categories']]
-    data.columns = ['Name', 'Address', 'Latitude', 'Longitude', 'Distance', 'City', 'Categories']
-    data['Categories'] = data['Categories'].apply(lambda x: x[0]['name'])
-    data = data[['Name', 'Categories', 'Distance', 'Address', 'City', 'Latitude', 'Longitude']]
-    data.sort_values(by = ['Distance'], inplace = True)
-    #st.dataframe(data)
+        data = data[['venue.name', 'venue.location.address', 'venue.location.lat', 'venue.location.lng', 'venue.location.distance', 'venue.location.city', 'venue.categories']]
+        data.columns = ['Name', 'Address', 'Latitude', 'Longitude', 'Distance', 'City', 'Categories']
+        data['Categories'] = data['Categories'].apply(lambda x: x[0]['name'])
+        data = data[['Name', 'Categories', 'Distance', 'Address', 'City', 'Latitude', 'Longitude']]
+        data.sort_values(by = ['Distance'], inplace = True)
+        #st.dataframe(data)
 
-    st.subheader('\nTOP 10 CLOSE ATTRACTIONS')
-    venues = pd.DataFrame(data.Categories.value_counts()).reset_index() # .transpose())
-    venues.rename(columns = {'index': 'Venue', 'Categories': 'Frequency'}, inplace = True)
-    # st.write(venues) 
-    
-    for i in range(10):
-        st.write(i+1, venues.Venue[i], venues.Frequency[i])
-    
-    
-    st.subheader('\n')
-    
-    category = st.text_input("\nEnter Category: ")
-    # data.sort_values(by = ['Distance'], inplace = True)
-    search = data[data.Categories == category]
-    search.sort_values(by = ['Distance'], inplace = True)
-    search = search.reset_index()
-    search = search.drop('index', axis = 1)   
-    # st.dataframe(search)
-    
-    for i in range(search.shape[0]):
-        st.write(i+1, search.Name[i], search.Categories[i], search.Distance[i])
-    
-    
-    """
-    incidents = folium.map.FeatureGroup()
+        st.subheader('\nTOP 10 CLOSE ATTRACTIONS')
+        venues = pd.DataFrame(data.Categories.value_counts()).reset_index() # .transpose())
+        venues.rename(columns = {'index': 'Venue', 'Categories': 'Frequency'}, inplace = True)
+        # st.write(venues) 
+        
+        for i in range(10):
+            st.write(i+1, venues['Venue'][i], venues['Frequency'][i])
+        
+        
+        st.subheader('\n')
+        
+        category = st.text_input("\nEnter Category: ")
+        # data.sort_values(by = ['Distance'], inplace = True)
+        search = data[data.Categories == category]
+        search.sort_values(by = ['Distance'], inplace = True)
+        search = search.reset_index()
+        search = search.drop('index', axis = 1)   
+        # st.dataframe(search)
+        
+        for i in range(search.shape[0]):
+            st.write(i+1, search.Name[i], search.Categories[i], search.Distance[i])
+        
+        
+        """
+        incidents = folium.map.FeatureGroup()
 
-    for lat, lng, in zip(data.Latitude, data.Longitude):
-        incidents.add_child(
-            folium.CircleMarker(
-                [lat, lng],
-                radius=5, # define how big you want the circle markers to be
-                color='yellow',
-                fill=True,
-                fill_color='red',
-                fill_opacity=0.6
+        for lat, lng, in zip(data.Latitude, data.Longitude):
+            incidents.add_child(
+                folium.CircleMarker(
+                    [lat, lng],
+                    radius=5, # define how big you want the circle markers to be
+                    color='yellow',
+                    fill=True,
+                    fill_color='red',
+                    fill_opacity=0.6
+                )
             )
-        )
 
-    # add pop-up text to each marker on the map
-    latitudes = list(data.Latitude)
-    longitudes = list(data.Longitude)
-    labels = list(data.Name)
+        # add pop-up text to each marker on the map
+        latitudes = list(data.Latitude)
+        longitudes = list(data.Longitude)
+        labels = list(data.Name)
 
-    for lat, lng, label in zip(latitudes, longitudes, labels):
-        folium.Marker([lat, lng], popup=label).add_to(Map)
+        for lat, lng, label in zip(latitudes, longitudes, labels):
+            folium.Marker([lat, lng], popup=label).add_to(Map)
 
-    # add incidents to map
-    Map.add_child(incidents)
-    """
-    
-    
-    '''
-    venues_map = folium.Map(location=[latitude, longitude], zoom_start=16) # generate map centred around the Grand Central Terminal
+        # add incidents to map
+        Map.add_child(incidents)
+        """
+        
+        
+        '''
+        venues_map = folium.Map(location=[latitude, longitude], zoom_start=16) # generate map centred around the Grand Central Terminal
 
-    # add a red circle marker to represent Grand Central Terminal
-    folium.CircleMarker(
-        [latitude, longitude],
-        radius=10,
-        color='red',
-        popup='Grand Central Terminal',
-        fill = True,
-        fill_color = 'red',
-        fill_opacity = 0.6
-    ).add_to(venues_map)
-
-    # add the pizza joints as blue circle markers
-    for lat, lng, label in zip(data.Latitude, data.Longitude, data.Categories):
-        print(lat, lng, label)
+        # add a red circle marker to represent Grand Central Terminal
         folium.CircleMarker(
-            [lat, lng],
-            radius=5,
-            color='blue',
-            popup=label,
+            [latitude, longitude],
+            radius=10,
+            color='red',
+            popup='Grand Central Terminal',
             fill = True,
-            fill_color='blue',
-            fill_opacity=0.6
+            fill_color = 'red',
+            fill_opacity = 0.6
         ).add_to(venues_map)
 
-    # display map
-    st.map(venues_map)
-    '''
-    
-    '''
-    gmaps = googlemaps.Client(key='AIzaSyBChwx9C-o1f_BwdGU1MAmfNwzorqjUoTU')
+        # add the pizza joints as blue circle markers
+        for lat, lng, label in zip(data.Latitude, data.Longitude, data.Categories):
+            print(lat, lng, label)
+            folium.CircleMarker(
+                [lat, lng],
+                radius=5,
+                color='blue',
+                popup=label,
+                fill = True,
+                fill_color='blue',
+                fill_opacity=0.6
+            ).add_to(venues_map)
 
-    # Geocoding an address
-    geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+        # display map
+        st.map(venues_map)
+        '''
+        
+        '''
+        gmaps = googlemaps.Client(key='AIzaSyBChwx9C-o1f_BwdGU1MAmfNwzorqjUoTU')
 
-    # Look up an address with reverse geocoding
-    reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+        # Geocoding an address
+        geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 
-    # Request directions via public transit
-    now = datetime.now()
-    directions_result = gmaps.directions("Sydney Town Hall",
-                                        "Parramatta, NSW",
-                                        mode="transit",
-                                        departure_time=now)
+        # Look up an address with reverse geocoding
+        reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 
-    # Validate an address with address validation
-    addressvalidation_result =  gmaps.addressvalidation(['1600 Amphitheatre Pk'], 
-                                                        regionCode='US',
-                                                        locality='Mountain View', 
-                                                        enableUspsCass=True)
-    '''
+        # Request directions via public transit
+        now = datetime.now()
+        directions_result = gmaps.directions("Sydney Town Hall",
+                                            "Parramatta, NSW",
+                                            mode="transit",
+                                            departure_time=now)
+
+        # Validate an address with address validation
+        addressvalidation_result =  gmaps.addressvalidation(['1600 Amphitheatre Pk'], 
+                                                            regionCode='US',
+                                                            locality='Mountain View', 
+                                                            enableUspsCass=True)
+        '''
