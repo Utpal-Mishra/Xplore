15c3063106c4b5921723b4a076bc81705a273b09
Utpal Mishra <utpalmishra21official@gmail.com>
2024-06-13 02:29:37 +0100

    Checking Folium Map With All Data

--- home.py
+++ home.py
@@ -274,14 +274,16 @@ def app():
             try:
                 if not search.empty():
                     map = search.rename(columns = {'Latitude': 'latitude', 'Longitude': 'longitude'})
+                    st.map(map, size = 100, zoom = 12)
                 else:
                     map = data.rename(columns = {'Latitude': 'latitude', 'Longitude': 'longitude'})
+                    st.map(map, size = 100, zoom = 12)
             except:
                 st.warning('Data Warning', icon="⚠️")
                 
             # st.dataframe(map)
             
-            st.map(map, size = 100, zoom = 12) # latitude = 'latitude', longitude = 'longitude', size=100, color='#0044ff'
+            # st.map(map, size = 100, zoom = 12) # latitude = 'latitude', longitude = 'longitude', size=100, color='#0044ff'
             
             ###################################################################################################
             
